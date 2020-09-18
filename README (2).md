0452ca643515be64515c383d3399ac8539d50a4e
Dip Chowdhury <dipbd1@gmail.com>
2020-09-18 14:59:24 +0600

    Initial commit

---
+++ README (2).md
@@ -0,0 +1,23 @@
+﻿**
+
+# Hello There,
+
+
+I am **Dip** and I was practicing ***kafka***. If everything Goes  well,
+it's highly possible I am now at architect level where I dont need to code anymore. 
+Thats **github**, for reminding me I can code!
+
+## So,
+Here run 
+
+    node producer.js
+
+to make some topic and inject data on them.
+
+And, run 
+
+    consumer.js
+
+to have a realtime watch on them.
+
+Wallah!! So easy. But people are now using kafka js instead of kafka-node!
---
+++ app_json.js
@@ -0,0 +1,25 @@
+const jsonData = [
+  {
+    name: 'Dip Chowdhury',
+    age: 22,
+    occupation: 'Software Engineer - Server Backend',
+    religion: 'No Relegion',
+    sports: 'Badminton',
+  },
+  {
+    name: 'Stick Man',
+    age: 25,
+    occupation: 'Project Manager',
+    religion: 'Nimai Brothers',
+    sports: 'Has not aquired yet',
+  },
+  {
+    name: 'Cyka Blyat',
+    age: 26,
+    occupation: 'Full Stack Software Engineer',
+    religion: 'Islam for sure',
+    sports: 'Has not aquired yet',
+  },
+];
+
+module.exports = jsonData;
